0b93c715e766c4c0ceb13e97fac4c00a51ecc3db
David Vitrant <david@synteny.us>
2013-10-25 20:14:41 -0400

    added some horizontal and vertial sliders for pan-tilt

--- static/javascript/tornado_servo2.js
+++ static/javascript/tornado_servo2.js
@@ -1,16 +1,49 @@
 $(document).ready(function(){
 	var ws;
+
 	ws = new WebSocket("ws://localhost:8888/ws");
+
+	$(function() {
+	   $( "#vertical-slider" ).slider({
+	   	 orientation: "vertical",
+	     value:90,
+	     min: 0,
+	     max: 180,
+	     step: 15,
+	     slide: function( event, ui ) {
+	       $( "input.vertical-angle" ).val( "vertical-" + ui.value );
+			ws.send(ui.value);
+	     }
+	   });
+	
+//	   $( "#amount" ).val( "$" + $( "#slider" ).slider( "value" ) );
+	});
+
+	$(function() {
+	   $( "#horizontal-slider" ).slider({
+	     value:90,
+	     min: 0,
+	     max: 180,
+	     step: 15,
+	     slide: function( event, ui ) {
+	       $( "input.horiz-angle" ).val( "horiz-" + ui.value );
+			ws.send(ui.value);
+	     }
+	   });
+	});
 
 	ws.onopen = function(evt) { 
 		$(".angle").css("background", "#00ff00"); 
 		alert("Connection openned!");
 	};
 
-	$(".angle").change(function(){
-		var angle = $("input.angle").val();
-		ws.send(angle);
-	});
 
-	
 });
+
+/* Potential Stringify code
+
+websocket.send(JSON.stringify({
+  id: "client1"
+}));
+
+*/
